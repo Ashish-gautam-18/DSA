4b724a29495e38549af0d2abf6c927aee8f3b988
ASHISH GAUTAM <ashish.gautam.254681@gmail.com>
2026-06-17 13:04:57 +0530

    Add files via upload

---
+++ Pattern/Pattern4.java
@@ -0,0 +1,18 @@
+package com.pattern.loop;
+
+public class Pattern4 {
+
+	public static void main(String[] args) {
+		
+		
+		for (int i=1;i<=5; i++) {
+			char ch='A';                                                   
+			for (int j=1;j<=5;j++) {
+             
+				System.out.print(ch++);
+				
+		}
+System.out.println();
+}
+}
+	}
