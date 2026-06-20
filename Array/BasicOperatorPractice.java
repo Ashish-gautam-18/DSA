7b53e3dc0ef08a1bf5b0b7e06bfcbc952dcd9ec7
ASHISH GAUTAM <ashish.gautam.254681@gmail.com>
2026-06-20 22:31:10 +0530

    Add files via upload

---
+++ Array/BasicOperatorPractice.java
@@ -0,0 +1,116 @@
+package web.string.array;
+
+public class BasicOperatorPractice {
+
+
+		public static void main(String[] args) {
+			
+			int rem;
+			int [] arr= { 4,8,10,15,19,18};
+			for(int a:arr) {
+				rem = a%2;
+				a=a/2;
+				System.out.println(rem);
+			}
+
+	}
+		}
+
+			
+//			
+//			int [] a= {0,2,4,1,3};
+//	             for (int i= 0; i<a.length;i++) {
+//	            
+//	            	 a[i]=a[(a[i]+3)%a.length];
+//	            	 
+//	             }
+//	            	 System.out.println(a[1]);
+//	             }
+//		}
+
+//	     output
+
+//			
+//		int [] arr= {2,3,6,8,12,14};
+//		int sum=0;
+//		for (int a:arr) {
+	//sum = sum+a;
+//		}
+//			System.out.println(sum);
+	//
+	//	
+//		}	
+//		}        output	45
+
+		
+			
+			
+			
+			
+//			
+//			int[] arr = { 2, 3, 6, 8, 12, 14 };
+//			int square = 0;
+	//
+//			int sum = 0;
+//			for (int a : arr) {
+//				square = a * a;
+//				sum = sum + square;
+//			}
+//			System.out.println(sum);
+	//
+//		}                                                output---  0 1 2 3 4 5  
+	//}
+
+
+	//
+	//
+//			int[] arr = {2,14,76,87};
+//			int sum=0;
+//			for (int i=0; i<=arr.length-1;i++) {
+//				sum=sum+i;
+//			}
+//			System.out.println(sum);
+	//
+	//	
+	//}
+	//}                         
+
+
+
+	//
+	//
+	//
+//			int[] arr = { 2, 3, 6, 8, 12, 14 };
+	//	
+//			for (int i=0; i<=arr.length-1;i=i+2) {
+//				
+//				System.out.println(arr[i]);
+//				
+//		}
+	//}
+	//
+	//}
+	//
+
+
+
+		
+//		              //dublicate value 
+			
+			
+			
+//		int [] arr= { 2,3,4,2,3,6,7,8,6};
+	//	
+//		for (int i=0; i<arr.length; i++) {
+	//	
+//			for (int j=i+1; j<arr.length; j++) {
+//				if (arr[i]==arr[j]) 
+//				
+//		        	   System.out.println(arr[i]);
+//			}
+//		}
+//		                                                      
+//		}
+	//}
+		
+			
