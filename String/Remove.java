cf81b0d0f9a217b9e583bcf12a6f94442ddcff41
ASHISH GAUTAM <ashish.gautam.254681@gmail.com>
2026-06-23 14:23:35 +0530

    Add files via upload

---
+++ String/Remove.java
@@ -0,0 +1,41 @@
+
+ import java.util.*;   
+public class Remove {
+//
+	public static void main(String[] args) {
+		
+		     String letters = "alpha, beta, gama, delta, sigma";
+
+		        // splitting the letters string using comma delimiter.
+		        String result[] = letters.split(",");
+
+		        //using java loop to print elements of string array 
+		       for (int i = 0; i < result.length; i++) {
+		            System.out.print(result[i]);
+		       }
+		    }
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+		
+		// REMOVE ALL SPACE FORM STRING WITH METHOD..........
+
+
+
+		
+		
+ 
+		 
+
+    	
+
